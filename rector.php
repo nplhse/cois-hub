bb68769482e5ee87ab12fd293884bae9e1464ece
Christoph Steltner <mail@nplhse.com>
2023-12-18 16:19:56 +0100

    build(dev): Improved rector settings

--- rector.php
+++ rector.php
@@ -2,16 +2,15 @@
 
 declare(strict_types=1);
 
-use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
-use Rector\Set\ValueObject\LevelSetList;
 use Rector\Doctrine\Set\DoctrineSetList;
+use Rector\Set\ValueObject\LevelSetList;
 use Rector\Symfony\Set\SymfonySetList;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
-        __DIR__ . '/src',
-        __DIR__ . '/tests',
+        __DIR__.'/src',
+        __DIR__.'/tests',
     ]);
 
     // Path to Symfony configuration
@@ -19,9 +18,6 @@ return static function (RectorConfig $rectorConfig): void {
 
     // Path to Symfony container
     $rectorConfig->symfonyContainerPhp(__DIR__.'/tests/symfony-container.php');
-
-    // Path to PHPStan with extensions, that PHPStan in Rector uses to determine types
-    $rectorConfig->phpstanConfig(__DIR__.'/phpstan.neon.dist');
 
     $rectorConfig->sets([
         LevelSetList::UP_TO_PHP_82,
--- tests/symfony-container.php
+++ tests/symfony-container.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 use App\Kernel;
 
-$appKernel = new Kernel('tests', false);
+require __DIR__.'/bootstrap.php';
+
+$appKernel = new Kernel('test', false);
 $appKernel->boot();
 
 return $appKernel->getContainer();
