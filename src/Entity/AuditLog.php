e0ff67487046e064dc9b423936c3dd735176c1aa
Christoph Steltner <mail@nplhse.com>
2023-12-18 16:25:28 +0100

    style: Updated entity with rector

--- src/Entity/AuditLog.php
+++ src/Entity/AuditLog.php
@@ -13,38 +13,14 @@ class AuditLog
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 255)]
-    private string $entityType;
-
-    #[ORM\Column]
-    private int $entityId;
-
-    #[ORM\Column]
-    private \DateTimeImmutable $createdAt;
-
-    #[ORM\Column(length: 255, nullable: true)]
-    private ?string $action = null;
-
-    #[ORM\Column(length: 255)]
-    private string $requestRoute;
-
-    #[ORM\Column(length: 255, nullable: true)]
-    private ?string $ipAddress = null;
-
-    public function __construct(
-        string $entityType,
-        int $entityId,
-        \DateTimeImmutable $createdAt,
-        string $requestRoute,
-        string $action = null,
-        string $ipAddress = null
-    ) {
-        $this->entityType = $entityType;
-        $this->entityId = $entityId;
-        $this->createdAt = $createdAt;
-        $this->action = $action;
-        $this->requestRoute = $requestRoute;
-        $this->ipAddress = $ipAddress;
+    public function __construct(#[ORM\Column(length: 255)]
+        private string $entityType, #[ORM\Column]
+        private int $entityId, #[ORM\Column]
+        private \DateTimeImmutable $createdAt, #[ORM\Column(length: 255)]
+        private string $requestRoute, #[ORM\Column(length: 255, nullable: true)]
+        private ?string $action = null, #[ORM\Column(length: 255, nullable: true)]
+        private ?string $ipAddress = null)
+    {
     }
 
     public function getId(): ?int
