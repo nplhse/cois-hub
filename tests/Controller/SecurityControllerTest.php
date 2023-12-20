11c152c23f4b74c50a789f7638b3f742d1f4d474
Christoph Steltner <mail@nplhse.com>
2023-12-20 14:40:52 +0100

    test: Added SecurityController test

---
+++ tests/Controller/SecurityControllerTest.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Application\Controller;
+
+use App\Factory\UserFactory;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Zenstruck\Browser\Test\HasBrowser;
+use Zenstruck\Foundry\Test\Factories;
+use Zenstruck\Foundry\Test\ResetDatabase;
+
+class SecurityControllerTest extends WebTestCase
+{
+    use HasBrowser;
+    use ResetDatabase;
+    use Factories;
+
+    public function testLoginLogout(): void
+    {
+        UserFactory::new(['username' => 'foo'])->create();
+
+        $this->browser()
+            ->visit('/login')
+            ->assertSeeIn('title', 'Login')
+            ->assertSeeIn('h2', 'Login')
+            ->fillField('Username', 'foo')
+            ->fillField('Password', 'password')
+            ->click('Sign in')
+            ->assertSuccessful()
+            ->assertSeeIn('#user_name', 'foo')
+            ->assertNotSee('Login')
+            ->visit('/logout')
+            ->assertNotSee('foo')
+        ;
+    }
+}
