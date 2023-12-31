29a588da681b92e1d2dce33f679da28ea6a71768
Christoph Steltner <mail@nplhse.com>
2023-12-31 19:08:07 +0100

    test: Added most of the routes to this test

--- tests/Integration/AppRoutesTest.php
+++ tests/Integration/AppRoutesTest.php
@@ -37,16 +37,31 @@ class AppRoutesTest extends WebTestCase
         );
     }
 
+    public function testLogoutUrlRedirects(): void
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/logout');
+        $this->assertResponseRedirects(
+            '/',
+            Response::HTTP_FOUND
+        );
+    }
+
     public function getPublicUrls(): ?\Generator
     {
         yield 'app_homepage' => ['/'];
         yield 'app_login' => ['/login'];
         yield 'app_register' => ['/register'];
+        yield 'app_forgot_password_request' => ['/reset-password'];
+        yield 'app_check_email' => ['reset-password/check-email'];
+        yield 'app_user' => ['/user/'];
     }
 
     public function getSecureUrls(): ?\Generator
     {
         yield 'app_admin_dashboard' => ['/admin/'];
         yield 'app_settings' => ['/settings'];
+        yield 'app_settings_password' => ['/settings/password'];
     }
 }
