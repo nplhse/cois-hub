64d5010cdb7446133b6a5d728a1b2c8b066d4042
Christoph Steltner <mail@nplhse.com>
2024-01-04 22:16:10 +0100

    build(deps): Added symfony/ux-components

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
+    Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
 ];
