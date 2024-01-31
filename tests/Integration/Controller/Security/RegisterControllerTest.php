02ff59086411ed0af5003afc0cc06c265a9f3848
Christoph Steltner <mail@nplhse.com>
2024-01-31 16:22:54 +0100

    style: Fixed small issue

--- tests/Integration/Controller/Security/RegisterControllerTest.php
+++ tests/Integration/Controller/Security/RegisterControllerTest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Tests\Integration\Controller\Security;
 
-use App\Factory\UserFactory;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Zenstruck\Browser\Test\HasBrowser;
 use Zenstruck\Foundry\Test\Factories;
